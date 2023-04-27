afa23d57e9a4fe6f2952a2f403631677473ee3bb
nwb8 <nwb8@fresnou.org>
2023-04-27 08:36:13 -0700

    initial commit

--- Character.js
+++ Character.js
@@ -71,4 +71,84 @@ function displayChar(char){
     console.log(`MAS: ${char.mastery}`);
     console.log(': ${char.}');
     console.log('-------------------------');
-} 
+}
+//2
+function createChar(character, list){
+    character.name = readLine.question("Enter character name: ");
+    console.log("Rolling STR");
+    character.strength = rollStat(diceRolled);
+    console.log("Rolling DEX");
+    character.dexerity = rollStat(diceRolled);
+    console.log("Rolling CON");
+    character.constitution = rollStat(diceRolled);
+    console.log("Rolling INT");
+    character.intelligence = rollStat(diceRolled);
+    console.log("Rolling WIS");
+    character.wisdom = rollStat(diceRolled);
+    console.log("Rolling CHA");
+    character.charisma = rollStat(diceRolled);
+    console.log("Rolling MA");
+    character.mana = rollStat(diceRolled);
+    console.log("Rolling SPE");
+    character.speed = rollStat(diceRolled);
+    console.log("Rolling MAS");
+    character.mastery = rollStat(diceRolled);
+    console.log(character);
+    displayChar(character);
+}
+
+function displayChar(char){
+    console.log('-----------------------');
+    console.log(`Name: ${char.name}`);
+    console.log(`STR: ${char.strength}`);
+    console.log(`DEX: ${char.dexerity}`);
+    console.log(`CON: ${char.constitution}`);
+    console.log(`INT: ${char.intelligence}`);
+    console.log(`WIS: ${char.wisdom}`);
+    console.log(`CHA: ${char.charisma}`);
+    console.log(`MA: ${char.mana}`);
+    console.log(`SPE: ${char.speed}`);
+    console.log(`MAS: ${char.mastery}`);
+    console.log(': ${char.}');
+    console.log('-------------------------');
+}
+//3
+function createChar(character, list){
+    character.name = readLine.question("Enter character name: ");
+    console.log("Rolling STR");
+    character.strength = rollStat(diceRolled);
+    console.log("Rolling DEX");
+    character.dexerity = rollStat(diceRolled);
+    console.log("Rolling CON");
+    character.constitution = rollStat(diceRolled);
+    console.log("Rolling INT");
+    character.intelligence = rollStat(diceRolled);
+    console.log("Rolling WIS");
+    character.wisdom = rollStat(diceRolled);
+    console.log("Rolling CHA");
+    character.charisma = rollStat(diceRolled);
+    console.log("Rolling MA");
+    character.mana = rollStat(diceRolled);
+    console.log("Rolling SPE");
+    character.speed = rollStat(diceRolled);
+    console.log("Rolling MAS");
+    character.mastery = rollStat(diceRolled);
+    console.log(character);
+    displayChar(character);
+}
+
+function displayChar(char){
+    console.log('-----------------------');
+    console.log(`Name: ${char.name}`);
+    console.log(`STR: ${char.strength}`);
+    console.log(`DEX: ${char.dexerity}`);
+    console.log(`CON: ${char.constitution}`);
+    console.log(`INT: ${char.intelligence}`);
+    console.log(`WIS: ${char.wisdom}`);
+    console.log(`CHA: ${char.charisma}`);
+    console.log(`MA: ${char.mana}`);
+    console.log(`SPE: ${char.speed}`);
+    console.log(`MAS: ${char.mastery}`);
+    console.log(': ${char.}');
+    console.log('-------------------------');
+}
